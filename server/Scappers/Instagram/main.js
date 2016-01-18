54314c42b82c8d559fae5ab6111072808b183945
Erik O'Keefe <erik.t.okeefe@gmail.com>
2016-01-17 16:58:26 -0800

    Test collection
    Built test collection for testing connection from database to D3 graph.

--- server/Scappers/Instagram/main.js
+++ server/Scappers/Instagram/main.js
@@ -1,21 +1,21 @@
-var tag = "porsche"; // Tag in question
-
-Instagram_pag.find().observe({
-  added: function (doc) {
-    if (doc.next_max_id !== null) instaARCHIVE(tag, doc.next_max_id)
-    else {
-      console.log("DB is currently up to date.");
-      Meteor.setInterval(function () {instaUPDATE(tag,doc.next_min_id)}, 3000);
-    }
-  },
-  changed: function (doc) {
-    if (doc.next_max_id !== null) instaARCHIVE(tag, doc.next_max_id)
-    else {
-      console.log("DB is currently up to date.");
-      Meteor.setInterval(function () {instaUPDATE(tag,doc.next_min_id)}, 3000);
-    }
-  }
-});
-
-instaINFO(tag); // Collect information on tag
-instaARCHIVE(tag, '0'); // Collect arcived data
+// var tag = "porsche"; // Tag in question
+//
+// Instagram_pag.find().observe({
+//   added: function (doc) {
+//     if (doc.next_max_id !== null) instaARCHIVE(tag, doc.next_max_id)
+//     else {
+//       console.log("DB is currently up to date.");
+//       Meteor.setInterval(function () {instaUPDATE(tag,doc.next_min_id)}, 3000);
+//     }
+//   },
+//   changed: function (doc) {
+//     if (doc.next_max_id !== null) instaARCHIVE(tag, doc.next_max_id)
+//     else {
+//       console.log("DB is currently up to date.");
+//       Meteor.setInterval(function () {instaUPDATE(tag,doc.next_min_id)}, 3000);
+//     }
+//   }
+// });
+//
+// instaINFO(tag); // Collect information on tag
+// instaARCHIVE(tag, '0'); // Collect arcived data
