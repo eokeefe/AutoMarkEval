c7bd1e7238a20eb69db730dbb06777eb9470db1e
Erik O'Keefe <erik.t.okeefe@gmail.com>
2016-01-30 20:05:50 -0800

    Instagram for each loop
    Just the start :)

--- server/Processing/Packaging.js
+++ server/Processing/Packaging.js
@@ -1,13 +1,18 @@
-// var today = moment().unix();
-// var yesterday = moment().subtract(1, 'day').unix();
+/* Example of unix time calls
+Current time in unix => moment().unix();
+Yesterday in unix => moment().subtract(1, 'day').unix();
+*/
 
-console.log(moment().hour(0).unix());
-console.log(moment().subtract(1, 'day').unix());
-
+/* Example of finding number of tags within 24hrs of now
 var testCount = Instagram_db.find({
   'created_time': {
     $gte: String(moment().subtract(1, 'day').unix()),
     $lt: String(moment().unix())
   }}).count();
+*/
 
-console.log(testCount);
+// {}, {fields: {_id: 0}}
+/* NOT WORKING */
+Instagram_db.find({}, {fields: {created_time: 1}}).forEach(function (time) {
+  console.log(time);
+})
