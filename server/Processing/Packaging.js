304b7c20c8d8cff88a771c07ac5c377ec45b73c1
Erik O'Keefe <erik.t.okeefe@gmail.com>
2016-02-01 14:16:48 -0800

    Instagram is finally counting correctly!!!
    Note: currently not fully tested…

--- server/Processing/Packaging.js
+++ server/Processing/Packaging.js
@@ -2,22 +2,15 @@ Meteor.startup(function () { Tags_db.remove({}); }); // FOR PROTOTYPING ONLY!
 
 Instagram_db.find({}, {fields: {created_time: 1}}).forEach(function (doc) {
   var date = moment.unix(doc.created_time).format("YYYYMMD");
-  var tags = Tags_db.find({'_id': date}).fetch();
+  var tags = Tags_db.findOne({'_id': date});
 
-  /*
-  TODO: Current method to determin if to update or insert is not correct! 
-  */
-
-  console.log(tags);
-  if(tags.Instagram = 0) {
-    console.log("attempting to insert new ID at: " + date)
+  if(tags.Instagram === undefined) {
     Tags_db.insert({'_id': date, 'Instagram': 1}, function (err, id) {
       if(err == null) console.log("Entry " + id + " was created.");
+      else console.log(err);
     });
   } else {
-    // console.log(tags);
-    console.log(tags.Instagram);
-    tags = tags.Instagram + 1;
-    Tags_db.update({'_id': date}, {'Instagram': tags});
+    tags.Instagram = tags.Instagram++;
+    Tags_db.update({'_id': date}, {'Instagram': tags.Instagram});
   }
 })
