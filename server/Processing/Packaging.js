0afa48bc7241fed54fc46b060afa09424f3a6a85
Erik O'Keefe <erik.t.okeefe@gmail.com>
2016-02-06 21:56:24 -0800

    Simplified Packaging.js
    Still could be done better, be we are heading the right direction!

--- server/Processing/Packaging.js
+++ server/Processing/Packaging.js
@@ -1,18 +1,13 @@
-// Instagram_db.find({}, {fields: {created_time: 1}}).forEach(function (doc) {
-//   var date = moment.unix(doc.created_time).format("YYYYMMDD");
-//   var tags = this.findOne({'_id': date});
-//
-//   try {
-//     if(tags === undefined) {
-//       this.insert({'_id': date, 'Instagram': 1}, function (err, id) {
-//         if(err == null) console.log("Entry " + id + " was created.");
-//         else console.log(err);
-//       });
-//     } else {
-//       tags.Instagram = tags.Instagram+1;
-//       this.update({'_id': date}, {'Instagram': tags.Instagram});
-//     }
-//   } catch (err) {throw err;}
-//
-//   return this;
-// }, Tags_db)
+Tags_db.remove({}); // General Information collections
+
+Instagram_db.find({}, {fields: {created_time: 1}}).forEach(function (doc) {
+  var date = moment.unix(doc.created_time).format("YYYYMMDD");
+  var tags = this.findOne({'_id': date});
+
+  if(tags === undefined) {
+    this.insert({'_id': date, 'Instagram': 1}, function (err, id) {
+      try { console.log("Entry " + id + " was created."); }
+      catch (err) { console.log(err); }
+    });
+  } else { this.update({'_id': date}, {$inc: {'Instagram': 1}}); }
+}, Tags_db)
