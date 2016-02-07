6d3686fd1dd3b40c106e60b6b56c5bf0644355d8
Erik O'Keefe <erik.t.okeefe@gmail.com>
2016-02-06 18:43:00 -0800

    Server is rendering correctly
    The server is counting correctly, however, rendering to the client
    isn’t work correctly.
    
    Next: Find why d3 is not being mapped correctly.

--- server/Processing/Packaging.js
+++ server/Processing/Packaging.js
@@ -1,16 +1,18 @@
-Meteor.startup(function () { Tags_db.remove({}); }); // FOR PROTOTYPING ONLY!
-
-Instagram_db.find({}, {fields: {created_time: 1}}).forEach(function (doc) {
-  var date = moment.unix(doc.created_time).format("YYYYMMD");
-  var tags = Tags_db.findOne({'_id': date});
-
-  if(tags.Instagram === undefined) {
-    Tags_db.insert({'_id': date, 'Instagram': 1}, function (err, id) {
-      if(err == null) console.log("Entry " + id + " was created.");
-      else console.log(err);
-    });
-  } else {
-    tags.Instagram = tags.Instagram++;
-    Tags_db.update({'_id': date}, {'Instagram': tags.Instagram});
-  }
-})
+// Instagram_db.find({}, {fields: {created_time: 1}}).forEach(function (doc) {
+//   var date = moment.unix(doc.created_time).format("YYYYMMDD");
+//   var tags = this.findOne({'_id': date});
+//
+//   try {
+//     if(tags === undefined) {
+//       this.insert({'_id': date, 'Instagram': 1}, function (err, id) {
+//         if(err == null) console.log("Entry " + id + " was created.");
+//         else console.log(err);
+//       });
+//     } else {
+//       tags.Instagram = tags.Instagram+1;
+//       this.update({'_id': date}, {'Instagram': tags.Instagram});
+//     }
+//   } catch (err) {throw err;}
+//
+//   return this;
+// }, Tags_db)
