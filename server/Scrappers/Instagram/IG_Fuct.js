04729cb965c40367922ec46bdcf8e63111e6c1be
Erik O'Keefe <erik.t.okeefe@gmail.com>
2016-02-13 02:42:40 -0800

    new db struct and insta func() update
    instaARCHIVE should be using multi insert not a for loop… Currently not
    working properly.

--- server/Scrappers/Instagram/IG_Fuct.js
+++ server/Scrappers/Instagram/IG_Fuct.js
@@ -16,53 +16,17 @@ instaINFO = function (id) {
 };
 
 /* Instagram to DB */
-instaARCHIVE = function (tag, id) {
-  /* Undefined Check */
-  if (tag === undefined) {
-    console.log("tag not defined");
-    return
-  }
+instaARCHIVE = function (request, id) {
+  instagram.tags.media(request, {max_tag_id: id}, function (tag, err, pag) {
+    Fiber(function() {
+      for (i = 0; i < tag.length; ++i) {
+        Instagram_db.insert(tag);
+      };
+      console.log(Instagram_db.find().count());
 
-  /* Gather Instagram Tags */
-  instagram.tags.media(tag, {max_tag_id: id}, function (tag, err, pag) {
-    try {
-      Fiber(function() {
-        /* Tag Collections */
-        for (i = 0; i < tag.length; ++i) {
-          Instagram_db.insert(tag[i]);
-        };
-
-        /* Tag Pagination */
-        var max = pag.next_max_id, min = pag.next_min_id; // Variables
-
-        if (Instagram_pag.find().count() === 0) { // Build Pagination
-          Instagram_pag.insert({next_max_id: max, next_min_id: min, type: "pagi"});
-        } else { // Update Pagination
-          Instagram_pag.update({type: "pagi"}, {next_max_id: max, next_min_id: min, type: "pagi"});
-        };
-      }).run(); // Execute Fiber
-    } catch (err) { throw err; }; // Error management
-  });
-};
-
-/* Instagram Update DB */
-instaUPDATE = function (tag, id) {
-  /* Undefined Check */
-  if (tag === undefined) {
-    console.log("tag not defined");
-    return
-  }
-
-  instagram.tags.media(tag, {max_tag_id: id}, function (tag, err, pag) {
-    try {
-      Fiber(function() {
-        /* Tag Collections */
-        for (i = 0; i < tag.length; ++i) {
-          if(Instagram_db.findOne({"images.standard_resolution.url": tag[i].images.standard_resolution.url}) === null) {
-            Instagram_db.insert(tag[i]);
-          }
-        };
-      }).run();
-    } catch (err) { throw err; }; // Error management
+      Interests.upsert({_id: request}, {
+        $set: {"instagram.pagi.min_id": pag.next_max_id, "instagram.pagi.max_id": pag.next_min_id}
+      });
+    }).run(); // Execute Fiber
   });
 };
--- server/Scrappers/Instagram/main.js
+++ server/Scrappers/Instagram/main.js
@@ -3,27 +3,21 @@ Meteor.startup(function () {
   /* Rebuild DB on Server Start */
   Tags_db.remove({}); // General Information collections
   Instagram_db.remove({}); // Instagram collections
-  Instagram_pag.remove({}); // Instagram pagination
+
+  Interests.find().forEach(function (doc) { instaINFO(doc._id); });
 });
 
-var tag = "porsche"; // Tag in question
+// Instagram_pag.find().observe({
+//   added: function (doc) {
+//     if (doc.next_max_id !== null) instaARCHIVE(tag, doc.next_max_id)
+//     else {
+//       console.log("DB is currently up to date.");
+//     }
+//   },
+//   changed: function (doc) {
+//     if (doc.next_max_id !== null) instaARCHIVE(tag, doc.next_max_id)
+//     else { console.log("DB is currently up to date."); }
+//   }
+// });
 
-Instagram_pag.find().observe({
-  added: function (doc) {
-    if (doc.next_max_id !== null) instaARCHIVE(tag, doc.next_max_id)
-    else {
-      console.log("DB is currently up to date.");
-      Meteor.setInterval(function () {instaUPDATE(tag,doc.next_min_id)}, 3000);
-    }
-  },
-  changed: function (doc) {
-    if (doc.next_max_id !== null) instaARCHIVE(tag, doc.next_max_id)
-    else {
-      console.log("DB is currently up to date.");
-      Meteor.setInterval(function () {instaUPDATE(tag,doc.next_min_id)}, 3000);
-    }
-  }
-});
-
-// instaINFO(); // Collect information on tags
-// instaARCHIVE(tag, '0'); // Collect arcived data
+instaARCHIVE(Interests.findOne()._id, '0'); // Collect arcived data
